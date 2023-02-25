b8834019f332377915f88978feb9c0b0f0cbab74
Leanh <datletb789@gmail.com>
2023-02-25 15:10:20 +0700

    get db

--- database/BTTH01_CSE485_ex.sql
+++ database/BTTH01_CSE485_ex.sql
@@ -9,11 +9,11 @@ SELECT * FROM baiviet,tacgia WHERE baiviet.ma_tgia = tacgia.ma_tgia AND tacgia.t
 SELECT baiviet.ma_bviet,baiviet.tieude,baiviet.ten_bhat,tacgia.ten_tgia,theloai.ten_tloai,baiviet.ngayviet from baiviet INNER JOIN tacgia on  baiviet.ma_tgia = tacgia.ma_tgia INNER JOIN theloai on baiviet.ma_tloai = theloai.ma_tloai
 
 -- Tìm thể loại có số bài viết nhiều nhất
-SELECT tacgia.ten_tgia, COUNT(baiviet.ma_tgia)  from baiviet,tacgia WHERE baiviet.ma_tgia = tacgia.ma_tgia GROUP BY tacgia.ma_tgia
+SELECT theloai.ten_tloai, COUNT(baiviet.ma_tloai) as soluong from baiviet INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai GROUP BY theloai.ma_tloai ORDER BY soluong DESC LIMIT 1
 
 
--- Tìm tác giả có số bài viết nhiều nhất
-SELECT tacgia.ten_tgia, COUNT(baiviet.ma_tgia)  from baiviet,tacgia WHERE baiviet.ma_tgia = tacgia.ma_tgia GROUP BY tacgia.ma_tgia
+-- Tìm 2 tác giả có số bài viết nhiều nhất
+SELECT tacgia.ten_tgia, COUNT(baiviet.ma_tgia) as soluong from baiviet INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia GROUP BY	tacgia.ma_tgia ORDER BY soluong DESC LIMIT 2
 
 -- Liệt kê các bài viết về các bài hát có tựa bài hát chứa 1 trong các từ “yêu”, “thương”, “anh”,“em”
 SELECT baiviet.ten_bhat FROM baiviet WHERE baiviet.ten_bhat LIKE "%yêu%" or baiviet.ten_bhat LIKE "%thương%" or baiviet.ten_bhat LIKE "%anh%" or baiviet.ten_bhat LIKE "%em%"
